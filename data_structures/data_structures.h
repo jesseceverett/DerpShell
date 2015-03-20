fda0349f403eafd06af636bef1eb903942a700b1
Vincent Moscatello <moscatello.vincent@gmail.com>
2015-03-19 21:49:14 -0400

    Working linked list!

--- data_structures/data_structures.h
+++ data_structures/data_structures.h
@@ -10,6 +10,17 @@ typedef struct linked_list{
 	struct node * end;
 } linked_list;
 
-void free_linked_list(linked_list * linked_list);
+//creates an instance of a linked list
+linked_list * create_linked_list();
+
+//frees the memory of a linked list
+void free_linked_list(linked_list * linkedlist);
+
+//adds an element to a linked list
+void push_linked_list(linked_list * linkedlist, char * data);
+
+//function used for debugging linked list. Simply traverse the linked list and print all its elements.
+void print_linked_list(linked_list * linkedlist);
+
 
 #endif
